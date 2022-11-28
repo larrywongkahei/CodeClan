f7659a14df6382f1c114dabb77bc41481303a5f4
Maryhei <makemak123@gmail.com>
2022-11-28 22:46:43 +0000

    Added all files

--- week_4/day_1/star_wars.sql
+++ week_4/day_1/star_wars.sql
@@ -1,17 +1,19 @@
-DROP TABLE characters;
 DROP TABLE lightsabers;
 
+DROP TABLE characters;
+
 CREATE TABLE characters(
-    id SERIAL,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     darkside BOOLEAN, 
     age INT
 );
 
 CREATE TABLE lightsabers(
-    id SERIAL, 
-    colour VARCHAR(255),
-    hilt_metal VARCHAR(255)
+    id SERIAL PRIMARY KEY, 
+    colour VARCHAR(255) NOT NULL,
+    hilt_metal VARCHAR(255) NOT NULL,
+    characters_id INT REFERENCES characters(id)
 );
 
 INSERT INTO characters (name, darkside, age) VALUES ('Obi-Wan Kenobi', true, 27);
@@ -24,14 +26,16 @@ INSERT INTO characters (name, darkside, age) VALUES ('Stormroope', false, 20);
 INSERT INTO characters (name, darkside, age) VALUES ('Stormroope', false, 20);
 INSERT INTO characters (name, darkside, age) VALUES ('Stormroope', false, 20);
 
-INSERT INTO lightsabers(colour, hilt_metal) VALUES ('green', 'palladium');
-INSERT INTO lightsabers(colour, hilt_metal) VALUES ('red', 'gold');
+INSERT INTO lightsabers(colour, hilt_metal, characters_id) VALUES ('green', 'palladium', 2);
+INSERT INTO lightsabers(colour, hilt_metal, characters_id) VALUES ('red', 'gold', 2);
+INSERT INTO lightsabers(colour, hilt_metal, characters_id) VALUES ('RED', 'COPPER', 3);
 
 
 
 
 SELECT * FROM lightsabers;
 SELECT * FROM characters;
+SELECT * FROM lightsabers WHERE characters_id = 3
 
 /* 
 To create table:
