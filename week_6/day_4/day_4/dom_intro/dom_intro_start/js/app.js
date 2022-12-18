8ccc411b9eab1dbce9a1acd8175915a65323f784
Maryhei <makemak123@gmail.com>
2022-12-18 19:04:13 +0000

    Done the day4 lab

--- week_6/day_4/day_4/dom_intro/dom_intro_start/js/app.js
+++ week_6/day_4/day_4/dom_intro/dom_intro_start/js/app.js
@@ -1,1 +1,53 @@
+console.log('app loaded', window);
 
+document.addEventListener('DOMContentLoaded', () => {
+    const fruits = ["apple", "orange", "banana"];
+	console.table(fruits);
+
+	const person = {
+		name: "Jane",
+		age: 40
+	};
+	console.table(person);
+    const title = document.querySelector('h1');
+    title.textContent = "Javascript says, BOO";
+    console.log('BOOO!')
+    const button = document.querySelector('#button'); // MODIFIED
+    button.addEventListener('click', handleButtonClick); // NEW
+    const textInput = document.querySelector('#input'); // NEW
+    textInput.addEventListener('input', handleInput); // NEW
+    const select = document.querySelector('#select');
+    select.addEventListener('input', handleSelect);
+    const form = document.querySelector('#form');
+    form.addEventListener('submit', handle_form)
+  });
+const handleButtonClick = function () {
+    const resultParagraph = document.querySelector('#button-result'); // NEW
+    resultParagraph.textContent = 'That button has definitely been clicked.';
+    const result = document.querySelector('#input-result');
+    result.textContent = 'You have type'; // NEW
+};
+
+const handleInput = function (event) {
+    console.log(event);
+}
+
+const handleSelect = function(event){
+    console.log('You went with ', event.target.value)
+}
+
+const handle_form = function(event){
+    event.preventDefault()
+    const form_data = document.querySelector('#form-result');
+    form_data.textContent = `Your name :${event.target.first_name.value} ${event.target.last_name.value}`;
+}
+
+// const handle_form = function (event) {
+//     event.preventDefault();
+//     const resultParagraph = document.querySelector('#form-result');
+//     resultParagraph.textContent = `
+//       Your name:
+//       ${event.target.first_name.value}
+//       ${event.target.last_name.value}
+//     `
+//   };
--- week_6/day_4/day_4/lab_events/lab_events_start/js/app.js
+++ week_6/day_4/day_4/lab_events/lab_events_start/js/app.js
@@ -1,3 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+  const save = document.querySelector('#new-item-form');
+  save.addEventListener('submit', handle_form);
+  const delete_button = document.createElement('button');
+  delete_button.textContent = "Delete All"
+  body = document.querySelector('body');
+  body.appendChild(delete_button);
+  delete_button.addEventListener('click', handle_delete)
 })
+const handle_form = function(event){
+  event.preventDefault();
+  const form_title = document.querySelector('#title');
+  const form_category = document.querySelector('#category');
+  const form_author = document.querySelector('#author');
+  const div = document.createElement('div');
+  const form_data = [form_title, form_author, form_category]
+  form_data.forEach((data) => {
+    const p = document.createElement('p');
+    p.textContent = data.value;
+    div.appendChild(p)
+    console.log(`done logging${data.value}`)
+  })
+  console.log('Loaded')
+  const reading_list = document.querySelector('#reading-list');
+  reading_list.appendChild(div)
+  document.querySelector('#new-item-form').reset();
+}
+
+const handle_delete = function(event){
+  event.preventDefault();
+  document.querySelector('#reading-list').textContent = '';
+  console.log('deleted everything')
+}
